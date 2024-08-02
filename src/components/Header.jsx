0e666301e58fd0dd505ba92496aa11a095698ee9
Chethan Kumar <chethankumars.v2016@gmail.com>
2024-08-02 23:43:03 +0530

    feat : header

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,86 +1,58 @@
+import "bootstrap/dist/css/bootstrap.min.css";
+import { FaHeart } from "react-icons/fa";
+import { CiShoppingCart } from "react-icons/ci";
+import { FaSearch } from "react-icons/fa";
+import "./Header.css";
+
 const Header = () => {
+  const cartItemCount = 1;
+
   return (
-    <div>
-      <nav className="navbar navbar-expand-lg bg-body-tertiary">
-        <div className="container-fluid">
-          <a className="navbar-brand" href="#">
-            Navbar
-          </a>
-          <button
-            className="navbar-toggler"
-            type="button"
-            data-bs-toggle="collapse"
-            data-bs-target="#navbarSupportedContent"
-            aria-controls="navbarSupportedContent"
-            aria-expanded="false"
-            aria-label="Toggle navigation"
-          >
-            <span className="navbar-toggler-icon"></span>
-          </button>
-          <div className="collapse navbar-collapse" id="navbarSupportedContent">
-            <ul className="navbar-nav me-auto mb-2 mb-lg-0">
-              <li className="nav-item">
-                <a className="nav-link active" aria-current="page" href="#">
-                  Home
-                </a>
-              </li>
-              <li className="nav-item">
-                <a className="nav-link" href="#">
-                  Link
-                </a>
-              </li>
-              <li className="nav-item dropdown">
-                <a
-                  className="nav-link dropdown-toggle"
-                  href="#"
-                  role="button"
-                  data-bs-toggle="dropdown"
-                  aria-expanded="false"
-                >
-                  Dropdown
-                </a>
-                <ul className="dropdown-menu">
-                  <li>
-                    <a className="dropdown-item" href="#">
-                      Action
-                    </a>
-                  </li>
-                  <li>
-                    <a className="dropdown-item" href="#">
-                      Another action
-                    </a>
-                  </li>
-                  <li>
-                    <hr className="dropdown-divider" />
-                  </li>
-                  <li>
-                    <a className="dropdown-item" href="#">
-                      Something else here
-                    </a>
-                  </li>
-                </ul>
-              </li>
-              <li className="nav-item">
-                <a className="nav-link disabled" aria-disabled="true">
-                  Disabled
-                </a>
-              </li>
-            </ul>
-            <form className="d-flex" role="search">
+    <nav className="navbar navbar-expand-lg navbar-light bg-body-tertiary ">
+      <div className="container">
+        <a
+          className="navbar-brand font-monospace text-decoration-underline fw-bold font-monospace fs-4"
+          href="#"
+        >
+          Drip&Flex
+        </a>
+        <button
+          className="navbar-toggler"
+          type="button"
+          data-toggle="collapse"
+          data-target="#navbarNav"
+          aria-controls="navbarNav"
+          aria-expanded="false"
+          aria-label="Toggle navigation"
+        >
+          <span className="navbar-toggler-icon"></span>
+        </button>
+        <div className="collapse navbar-collapse" id="navbarNav">
+          <form className="form-inline mx-auto">
+            <div className="search-container">
               <input
-                className="form-control me-2"
+                className="form-control search-input"
                 type="search"
-                placeholder="Search"
+                placeholder="Search for products"
                 aria-label="Search"
               />
-              <button className="btn btn-outline-success" type="submit">
-                Search
-              </button>
-            </form>
+              <FaSearch className="search-icon" />
+            </div>
+          </form>
+          <div className="icons">
+            <FaHeart
+              style={{ fontSize: "1.5rem", marginRight: "20px", color: "red" }}
+            />
+            <div className="cart-icon-container">
+              <CiShoppingCart style={{ fontSize: "1.5rem", color: "black" }} />
+              {cartItemCount > 0 && (
+                <span className="cart-item-count">{cartItemCount}</span>
+              )}
+            </div>
           </div>
         </div>
-      </nav>
-    </div>
+      </div>
+    </nav>
   );
 };
 
