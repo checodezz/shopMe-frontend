5b8703bc10c6e7f842386bee371c0921fa644ee4
Chethan Kumar <chethankumars.v2016@gmail.com>
2024-08-05 13:06:25 +0530

    fix : routes

--- src/App.jsx
+++ src/App.jsx
@@ -2,8 +2,6 @@ import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import "./App.css";
 import "bootstrap/dist/css/bootstrap.min.css";
 import "bootstrap/dist/js/bootstrap.bundle";
-// import Homepage from "./components/Homepage";
-// import Homepage from "./components/Homepage";
 import Homepage from "./components/Homepage";
 import Header from "./components/Header";
 import Products from "./components/Products";
@@ -17,7 +15,7 @@ function App() {
           {/* <Route to="/" element={<Homepage />} />
            */}
           <Route path="/" element={<Homepage />} />
-          <Route path="/products" element={<Products />} />
+          <Route path="/products/all" element={<Products />} />
         </Routes>
       </Router>
     </>
