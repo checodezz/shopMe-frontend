774e2dd460d02f9264830b5c9c82dbb48eb7cee3
Chethan Kumar <chethankumars.v2016@gmail.com>
2024-08-07 00:48:37 +0530

    feat : add productDetails slice to store

--- src/store/store.js
+++ src/store/store.js
@@ -1,9 +1,11 @@
 import { configureStore } from "@reduxjs/toolkit"
 import productSlice from "../features/products/productSlice"
+import productDetailsSlice from "../features/products/productDetailsSlice"
 
 const store = configureStore({
     reducer: {
-        products: productSlice
+        products: productSlice,
+        productDetails: productDetailsSlice
     }
 })
 
