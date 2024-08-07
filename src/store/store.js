938a0b871ec78409619b6f986b132a858d441e06
Chethan Kumar <chethankumars.v2016@gmail.com>
2024-08-07 14:39:23 +0530

    configure : wishlist reducer

--- src/store/store.js
+++ src/store/store.js
@@ -1,11 +1,13 @@
 import { configureStore } from "@reduxjs/toolkit"
 import productSlice from "../features/products/productSlice"
 import productDetailsSlice from "../features/products/productDetailsSlice"
+import wishlistSlice from "../features/wishlist/wishlistSlice"
 
 const store = configureStore({
     reducer: {
         products: productSlice,
-        productDetails: productDetailsSlice
+        productDetails: productDetailsSlice,
+        wishlist: wishlistSlice
     }
 })
 
