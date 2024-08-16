d412e5d7b4728e05d83249f5012e882fe0ec10f0
Chethan Kumar <chethankumars.v2016@gmail.com>
2024-08-16 11:46:30 +0530

    update: wishlist logic

--- src/features/wishlist/wishlistSlice.js
+++ src/features/wishlist/wishlistSlice.js
@@ -1,22 +1,58 @@
-import { createSlice, } from "@reduxjs/toolkit";
+import { createAsyncThunk, createSlice, } from "@reduxjs/toolkit";
+import { useSelector } from "react-redux";
+import axios from "axios";
+
+export const toggleWishlist = createAsyncThunk('wishlist/toggleWishlist', async (productId) => {
+    const response = await axios.post(`http://localhost:3000/wishlist/toggle/${productId}`);
+    return response.data;
+});
+
+// const wishListProduct = useSelector((state) => state.products.products).filter((product) => product.wishlist)
 
 export const wishlistSlice = createSlice({
     name: 'wishlist',
     initialState: {
-        products: []
+        products: [],
+        status: "idle",
+        error: null
     },
     reducers: {
-        addToWishlist: (state, action) => {
-            const productExists = state.products.find(product => product._id === action.payload._id);
-            if (!productExists) {
-                state.products.push(action.payload)
+        toggleWishlistOptimistic: (state, action) => {
+            const productIndex = state.products.findIndex(product => product._id === action.payload);
+            if (productIndex > -1) {
+                state.products[productIndex].wishlist = !state.products[productIndex].wishlist
             }
-        },
-        removeFromWishlist: (state, action) => {
-            state.products.filter(product => product._id !== action.payload._id)
-        }
-    }
-})
 
-export const { addToWishlist, removeFromWishlist } = wishlistSlice.actions
-export default wishlistSlice.reducer
+        },
+    },
+    extraReducers: (builder) => {
+        builder.addCase(toggleWishlist.pending, (state) => {
+            state.status = "pending";
+        })
+            .addCase(toggleWishlist.fulfilled, (state, action) => {
+                state.status = "success";
+                const updatedProduct = action.payload;
+
+                const productIndex = state.products.findIndex((product) => product._id === updatedProduct._id);
+
+                if (updatedProduct.wishlist) {
+                    if (productIndex > -1) {
+                        state.products[productIndex] = updatedProduct;
+                    } else {
+                        state.products.push(updatedProduct);
+                    }
+                } else {
+                    if (productIndex > -1) {
+                        state.products.splice(productIndex, 1); // Remove the product if wishlist is false
+                    }
+                }
+            })
+            .addCase(toggleWishlist.rejected, (state, action) => {
+                state.status = "failed";
+                state.error = action.error.message;
+            });
+    }
+});
+
+export const { addToWishlist, toggleWishlistOptimistic } = wishlistSlice.actions;
+export default wishlistSlice.reducer;
