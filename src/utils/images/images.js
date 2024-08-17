ccef347e4b41f751ef5604611341278154d1dfe9
Chethan Kumar <chethankumars.v2016@gmail.com>
2024-08-17 12:14:10 +0530

    create util for images

---
+++ src/utils/images/images.js
@@ -0,0 +1,9 @@
+export const BANNER_IMAGE = "https://listtopedia.com/wp-content/uploads/2023/09/Bandhan_Myntra_Offer_Banner_02.jpg"
+
+export const MEN_IMAGE = "https://img.freepik.com/free-photo/excited-man-holding-shopping-bags-looking-happy-mobile-phone-screen-standing-yellow-background_1258-40974.jpg"
+
+export const WOMEN_IMAGE = "https://media.istockphoto.com/id/1224234335/photo/conceptual-photo-of-happy-girl-holds-shopping-packages-on-blue-background.jpg?s=612x612&w=0&k=20&c=5bgmMIx7xbO6e61nd7nDUKY44P2zLU2IsTDfZ6RyI4g="
+
+export const KID_IMAGE = "https://t3.ftcdn.net/jpg/02/11/82/88/360_F_211828832_OnMaambs24g0vZM8HLjqZ8tU5wH4y1oD.jpg"
+
+export const FAMILY_IMAGE = "https://t3.ftcdn.net/jpg/02/96/36/60/360_F_296366036_gmfHjuby7EXTiheB434rWqBT06Hz8EOe.jpg"
