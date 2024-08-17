1218edb435b221668172ad92e703b57ce6459062
Chethan Kumar <chethankumars.v2016@gmail.com>
2024-08-17 12:13:44 +0530

    add css for homepage

--- src/pages/Homepage.jsx
+++ src/pages/Homepage.jsx
@@ -1,19 +1,50 @@
+import "../css/Homepage.css"; // Assuming you have a separate CSS file for styles
 import { Link } from "react-router-dom";
+import {
+  BANNER_IMAGE,
+  FAMILY_IMAGE,
+  KID_IMAGE,
+  MEN_IMAGE,
+  WOMEN_IMAGE,
+} from "../utils/images/images";
 
 const Homepage = () => {
+  const categories = [
+    { name: "Men", image: MEN_IMAGE, link: "/category/men" },
+    { name: "Women", image: WOMEN_IMAGE, link: "/category/women" },
+    { name: "Kids", image: KID_IMAGE, link: "/category/kids" },
+    { name: "Discover", image: FAMILY_IMAGE, link: "/products/all" },
+  ];
+
   return (
     <div>
-      <div className="">
+      <div className="container-fluid mb-5 ">
         <img
-          src="https://assets.myntassets.com/w_980,c_limit,fl_progressive,dpr_2.0/assets/images/2024/5/21/91b180cd-de53-4a07-8cd2-3e5b8d37efd41716229933950-Desktop-Banner.jpg"
+          src={BANNER_IMAGE}
           alt=""
-          className="container-fluid"
+          className="banner-image container-fluid m-0 p-0"
         />
       </div>
-      <div className="text-center">
-        <Link to="/products/all" className="btn btn-primary ">
-          Browse Products
-        </Link>
+
+      <div className="category-section">
+        <div className="row">
+          {categories.map((category, index) => (
+            <div className="col-md-3 px-2" key={index}>
+              <Link to={category.link} className="category-link">
+                <div className="category-card mx-4 rounded">
+                  <img
+                    src={category.image}
+                    alt={category.name}
+                    className="category-image"
+                  />
+                  <div className="category-overlay">
+                    <h2 className="category-text">{category.name}</h2>
+                  </div>
+                </div>
+              </Link>
+            </div>
+          ))}
+        </div>
       </div>
     </div>
   );
