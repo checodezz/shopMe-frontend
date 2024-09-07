ebfce55a1341b49bcaa52ded805df98a02a50881
Chethan Kumar <chethankumars.v2016@gmail.com>
2024-09-07 22:05:09 +0530

    Chore : Add dynamic images for clean code

--- src/pages/Homepage.jsx
+++ src/pages/Homepage.jsx
@@ -1,33 +1,48 @@
-import "../css/Homepage.css"; // Assuming you have a separate CSS file for styles
 import { Link } from "react-router-dom";
+import { useDispatch } from "react-redux";
+import { fetchProducts } from "../features/products/productSlice";
+import "../css/Homepage.css";
 import {
   BANNER_IMAGE,
   FAMILY_IMAGE,
   KID_IMAGE,
   MEN_IMAGE,
+  SHOP_BY_CATEGORY_IMAGE,
   WOMEN_IMAGE,
 } from "../utils/images/images";
 import Carousal from "../components/Carousal";
+import { useEffect } from "react";
 
 const Homepage = () => {
+  const dispatch = useDispatch();
+
+  useEffect(() => {
+    dispatch(fetchProducts());
+  }, []);
+
   const categories = [
+    { name: "All", image: FAMILY_IMAGE },
     { name: "Men", image: MEN_IMAGE },
     { name: "Women", image: WOMEN_IMAGE },
     { name: "Kids", image: KID_IMAGE },
-    { name: "All", image: FAMILY_IMAGE },
   ];
 
   return (
-    <div>
-      <div className="container-fluid mb-5 mt-3">
-        <img
-          src={BANNER_IMAGE}
-          alt=""
-          className="banner-image container-fluid m-0 p-0"
-        />
+    <div className="container">
+      <div className="container-fluid">
+        <Link to="/products/all">
+          <img
+            src={BANNER_IMAGE}
+            alt=""
+            className="banner-image container-fluid m-0 p-0"
+          />
+        </Link>
       </div>
 
       <div className="category-section">
+        <div>
+          <img src={SHOP_BY_CATEGORY_IMAGE} alt="" className="container mb-2" />
+        </div>
         <div className="row">
           {categories.map((category, index) => (
             <div className="col-md-3 px-2" key={index}>
@@ -50,7 +65,9 @@ const Homepage = () => {
           ))}
         </div>
       </div>
-      <Carousal />
+      <Link to="products/all">
+        <Carousal />
+      </Link>
     </div>
   );
 };
