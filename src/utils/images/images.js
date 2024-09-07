7b1a123afd550bac1bf8ed5dbe729f835f901e4e
Chethan Kumar <chethankumars.v2016@gmail.com>
2024-09-07 22:08:51 +0530

    feat: Add banner and category images for Men, Women, Kids, Family, and Carousel

--- src/utils/images/images.js
+++ src/utils/images/images.js
@@ -1,4 +1,4 @@
-export const BANNER_IMAGE = "https://listtopedia.com/wp-content/uploads/2023/09/Bandhan_Myntra_Offer_Banner_02.jpg"
+export const BANNER_IMAGE = "https://rukminim1.flixcart.com/flap/1800/1800/image/ddbe8a22de89bf94.jpg"
 
 export const MEN_IMAGE = "https://img.freepik.com/free-photo/excited-man-holding-shopping-bags-looking-happy-mobile-phone-screen-standing-yellow-background_1258-40974.jpg"
 
@@ -7,3 +7,9 @@ export const WOMEN_IMAGE = "https://media.istockphoto.com/id/1224234335/photo/co
 export const KID_IMAGE = "https://t3.ftcdn.net/jpg/02/11/82/88/360_F_211828832_OnMaambs24g0vZM8HLjqZ8tU5wH4y1oD.jpg"
 
 export const FAMILY_IMAGE = "https://t3.ftcdn.net/jpg/02/96/36/60/360_F_296366036_gmfHjuby7EXTiheB434rWqBT06Hz8EOe.jpg"
+
+export const CAROUSEL_IMAGE_1 = "https://lmspartnerprogram.com/wp-content/uploads/Corporate-Apparel-Brands-Banner.jpg"
+
+export const CAROUSEL_IMAGE_2 = "https://www.worldfashionexchange.com/blog/wp-content/uploads/2024/01/Fashion-Brands-Banner-image.webp"
+
+export const SHOP_BY_CATEGORY_IMAGE = "https://res.cloudinary.com/donqbxlnc/image/upload/v1648895557/fashify/0b21bba9-e1e2-4dd9-ac99-4a759abe68801648705771876-Shop-By-Category_w2adx7.webp"
