eba18b796b52f6bc82cd88e2b87a12c418ac5b3d
Chethan Kumar <chethankumars.v2016@gmail.com>
2024-09-07 21:59:28 +0530

    Chore : style footer for better ux

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -2,12 +2,21 @@ import { FaGithub } from "react-icons/fa";
 
 const Footer = () => {
   return (
-    <div className="p-3 bg-body-secondary text-center d-flex justify-content-between align-items-center">
-      <p className="mb-0">&copy; No Copyright, Feel free to replicate.</p>
-      <div>
-        <a href="https://github.com" target="_blank" rel="noopener noreferrer">
-          <FaGithub size={24} />
-        </a>
+    <div className="p-2 bg-body-secondary text-center d-flex justify-content-between align-items-center ">
+      <div className="container">
+        <p className="mb-0">
+          <strong>&copy; {new Date().getFullYear()} ShopMe.</strong> No
+          Copyright Feel free to replicate.
+        </p>
+        <div>
+          <a
+            href="https://github.com"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            <FaGithub size={24} />
+          </a>
+        </div>
       </div>
     </div>
   );
