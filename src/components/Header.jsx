419a623fd7b8292371f157023123d40fc8b99301
Chethan Kumar <chethankumars.v2016@gmail.com>
2024-09-08 15:25:10 +0530

    Fix : Responsiveness

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -44,6 +44,19 @@ const Header = () => {
           ShopMe
         </Link>
 
+        {/* Add the navbar-toggler button */}
+        <button
+          className="navbar-toggler"
+          type="button"
+          data-bs-toggle="collapse"
+          data-bs-target="#navbarNav"
+          aria-controls="navbarNav"
+          aria-expanded="false"
+          aria-label="Toggle navigation"
+        >
+          <span className="navbar-toggler-icon"></span>
+        </button>
+
         <div className="collapse navbar-collapse" id="navbarNav">
           <form className="form-inline mx-auto">
             <div className="search-container">
